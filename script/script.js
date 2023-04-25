448c3ad2742191881ca29522fc6464f33eb5baed
haiiron Mac <kirogroryhv@gmail.com>
2023-04-25 15:40:18 +0900

    example page

--- script/script.js
+++ script/script.js
@@ -1,35 +1,36 @@
-function request()
-{
-	const response = await fetch('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst',
-	{
-    	method: 'GET',
-	});
-  const data = await response.json();
-  console.log(data)
-}
-
 function getdata()
 {
-var xhr = new XMLHttpRequest();
-var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'; /*URL*/
-var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'서비스키'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000');
-queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML');
-queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent('20210628');
-queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent('0600');
-queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent('55');
-queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent('127');
-xhr.open('GET', url + queryParams);
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-    }
-};
+	var xhr = new XMLHttpRequest();
+	var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'; /*URL*/
+	var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'QgmUg9hj0Ehv%2BrHFMIkbEIhPUPuLChLl0vlVNzX3jXvo1tNPbOviKNFBigULWbdjFHdB66vZda2VlRGa7Putvw%3D%3D'; /*Service Key*/
+	queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+	queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000'); /**/
+	queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('json'); /**/
+	queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent('20230420'); /**/
+	queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent('0600'); /**/
+	queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent('55'); /**/
+	queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent('127'); /**/
+	xhr.open('GET', url + queryParams);
+	xhr.onreadystatechange = function ()
+	{
+		if (this.readyState == 4)
+		{
+			if(this.status == 200)
+			{
+				alert('상태 :'+ this.status + '\nBody: '+this.responseText);
+			}else {alert("something errors");}
+		}
+	};
 
-xhr.send('');
+	xhr.send('');
 }
 
-function changeValue()
+function showTime()
 {
+	const kr_time_dif = 32400000;
+	const sys_now = new Date();
+
+	const kr_curr = sys_now.getTime() + sys_now.getTimezoneOffset() * 60000;
 }
+
+getdata()
