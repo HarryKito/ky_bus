c191a596682286eba96c4927dceea18805bbd471
Haiiron <71598954+HarryKito@users.noreply.github.com>
2023-07-04 13:54:19 +0900

    Update bus system and time

--- script/script.js
+++ script/script.js
@@ -11,26 +11,51 @@ function getdata()
 	queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent('55'); /**/
 	queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent('127'); /**/
 	xhr.open('GET', url + queryParams);
-	xhr.onreadystatechange = function ()
-	{
+xhr.onreadystatechange = function ()
+ {
 		if (this.readyState == 4)
-		{
 			if(this.status == 200)
-			{
 				alert('상태 :'+ this.status + '\nBody: '+this.responseText);
-			}else {alert("something errors");}
-		}
+		else {alert("something errors");}
 	};
-
 	xhr.send('');
+	showTime();
 }
 
 function showTime()
 {
-	const kr_time_dif = 32400000;
-	const sys_now = new Date();
+	const curr = new Date();
+document.writeln("현재시간(Locale) : " + curr + '<br>');
 
-	const kr_curr = sys_now.getTime() + sys_now.getTimezoneOffset() * 60000;
+// 2. UTC 시간 계산
+const utc = curr.getTime() + (curr.getTimezoneOffset() * 60 * 1000);
+
+// 3. UTC to KST (UTC + 9시간)
+const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+const kr_curr = new Date(utc + (KR_TIME_DIFF));
+
+document.writeln("한국시간 : " + kr_curr);
 }
 
 getdata()
+
+function showBus()
+{
+	const fromUniv196 = [0,0];
+	const toUniv196 = [0,0];
+	const fromUniv74_1 =[0,0];
+	const toUniv74_1 = [0,0];
+	const fromUniv80 = [0,0];
+	const toUniv80 = [0,0];
+	const fromUniv883_1 = [0,0];
+	const toUniv883_1 = [0,0];
+	const fromUniv884_1 = [0,0];
+	const toUniv884_1 = [0,0];
+	const fromUniv891_1 = [0,0];
+	const toUniv891_1 = [0,0];
+	const fromUniv92_1 = [0,0];
+	const toUniv92_1 = [0,0];
+	const fromUniv92_2 = [0,0];
+	const toUniv92_2 = [0,0];
+	
+}
